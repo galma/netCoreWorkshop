ece1eebc4de897559efa23533cfcddc4629679b4
Guido Maximiliano Alma <galma@sparkdigital.com>
2017-09-11 16:55:49 -0300

    Task 1 - Add IIS and setup ContentRoot Directory

--- resolution/Program.cs
+++ resolution/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore;
+using System.IO;
 
 namespace resolution
 {
@@ -13,8 +14,8 @@ namespace resolution
             var host = new WebHostBuilder()
                 .UseKestrel()                
                 .UseStartup<Startup>()
-                //.UseIISIntegration() 
-                //.UseContentRoot(Directory.GetCurrentDirectory()) //  The server’s content root determines where it searches for content files, like MVC View files. The default content 
+                .UseIISIntegration() 
+                .UseContentRoot(Directory.GetCurrentDirectory()) //  The server’s content root determines where it searches for content files, like MVC View files. The default content 
                 .Build();
 
             host.Run();
